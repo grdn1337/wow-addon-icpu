6f7da2ebebe0d8da5b123af63a96d69ac8ee5428
grdn <grdn@51fe3c43-9e3c-4bab-bd8f-c332db245f8b>
2012-09-06 08:06:34 +0000

    - Added options UI, this is currently not finalized!

--- trunk/Locales/enUS.lua
+++ trunk/Locales/enUS.lua
@@ -5,4 +5,29 @@ local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "enUS", true)
 
 --@do-not-package@
 L["Addon update available!"] = true
+L["General Options"] = true
+L["Plugin Options"] = true
+L["Decimal digits"] = true
+L["The number of decimal digits for all numbers."] = true
+L["Update Interval"] = true
+L["The lower the value, the more CPU is needed!"] = true
+L["Display Memory"] = true
+L["Display FPS"] = true
+L["Display Latency"] = true
+L["Display CPU"] = true
+L["Displays the CPU time on the plugin when CPU profiling is enabled (Ctrl+Click)."] = true
+L["Tooltip Options"] = true
+
+
+L["mb"] = true
+L["kb"] = true
+L["kb/s"] = true
+L["ms"] = true
+L["fps"] = true
+L["Blizz UI"] = true
+L["Framerate"] = true
+L["Latency"] = true
+L["Download"] = true
+L["Upload"] = true
+
 --@end-do-not-package@
