5538165d7a384b6e49fc73a8fa77b5996111c36a
grdn <grdn@51fe3c43-9e3c-4bab-bd8f-c332db245f8b>
2016-08-14 10:14:02 +0000

    - See r26

---
+++ tags/7.0.2/Locales/enUS.lua
@@ -0,0 +1,38 @@
+local AddonName = ...;
+local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "enUS", true)
+
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
+
+--@do-not-package@
+L["Addon update available!"] = true
+L["General Options"] = true
+L["Plugin Options"] = true
+L["Decimal digits"] = true
+L["The number of decimal digits for all numbers."] = true
+L["Update Interval"] = true
+L["The lower the value, the more CPU is needed!"] = true
+L["Display Memory"] = true
+L["Display FPS"] = true
+L["Display Latency"] = true
+L["Display CPU"] = true
+L["Display Streaming"] = true
+L["Displays the CPU time on the plugin when CPU profiling is enabled (Ctrl+Click)."] = true
+L["Tooltip Options"] = true
+L["Number of Addons"] = true
+L["Setting this to 0 will disable listing addons."] = true
+
+L["Max height of tooltip"] = true
+L["Use Scrollbar"] = true
+L["Define the maximal height of the tooltip if you are using a scrollbar."] = true
+
+L["mb"] = true
+L["kb"] = true
+L["kb/s"] = true
+L["ms"] = true
+L["fps"] = true
+L["Blizz UI"] = true
+L["Framerate"] = true
+L["Latency"] = true
+L["Stream (U/D)"] = true
+
+--@end-do-not-package@
