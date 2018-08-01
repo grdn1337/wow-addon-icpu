18945730a0e46e18ad70037db0d9e22815dd76f0
grdn <grdn@51fe3c43-9e3c-4bab-bd8f-c332db245f8b>
2018-08-01 18:34:10 +0000

    See r39

---
+++ tags/8.0.2/Locales/deDE.lua
@@ -0,0 +1,39 @@
+local AddonName = ...;
+local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "deDE")
+
+if not L then return end
+
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
+
+--@do-not-package@
+L["Addon update available!"] = "Addon Update verf\195\188gbar!"
+L["Blizz UI"] = "Blizz UI"
+L["Decimal digits"] = "Nachkommastellen"
+L["Define the maximal height of the tooltip if you are using a scrollbar."] = "Bestimme die maximale Gr\195\182\195\159e des Tooltips, wenn die Scrollbar aktiviert ist."
+L["Display CPU"] = "Zeige CPU"
+L["Display FPS"] = "Zeige FPS"
+L["Display Latency Home"] = "Zeige Standort-Latenz"
+L["Display Latency World"] = "Zeige Welt-Latenz"
+L["Display Memory"] = "Zeige Speicher"
+L["Displays the CPU time on the plugin when CPU profiling is enabled (Ctrl+Click)."] = "Zeigt die CPU Zeit auf dem Plugin an, wenn CPU-Profiling aktiviert ist (Strg+Klick)."
+L["Display Streaming"] = "Zeige Streaming"
+L["fps"] = "fps"
+L["Framerate"] = "Framerate"
+L["General Options"] = "Allgemeine Optionen"
+L["kb"] = "kb"
+L["kb/s"] = "kb/s"
+L["Latency Home"] = "Standort-Latenz"
+L["Latency World"] = "Welt-Latenz"
+L["Max height of tooltip"] = "Maximale Gr\195\182\195\159e des Tooltips"
+L["mb"] = "mb"
+L["ms"] = "ms"
+L["Number of Addons"] = "Anzahl der Addons"
+L["Plugin Options"] = "Plugin Optionen"
+L["Setting this to 0 will disable listing addons."] = "Wenn dieser Wert 0 ist, werden keine Addons angezeigt."
+L["Stream (U/D)"] = "Stream (U/D)"
+L["The lower the value, the more CPU is needed!"] = "Je geringer der Wert, desto mehr CPU wird in Anspruch genommen."
+L["The number of decimal digits for all numbers."] = "Die Anzahl der Nachkommastellen f\195\188r alle Zahlen."
+L["Tooltip Options"] = "Tooltip Optionen"
+L["Update Interval"] = "Update Intervall"
+L["Use Scrollbar"] = "Scrollbar benutzen"
+--@end-do-not-package@
